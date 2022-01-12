49554f05ebf30b822eab078606bdd1e61d44a1c2
AdamSpera <64236429+AdamSpera@users.noreply.github.com>
2022-01-12 23:47:05 +0000

    Design Update

--- public/home.js
+++ public/home.js
@@ -3,6 +3,7 @@ var btnStart = document.getElementById('btnStart');
 var btnFinish = document.getElementById('btnFinish');
 var btnView = document.getElementById('btnView');
 var textDisplay = document.getElementById('textDisplay');
+var circleText = document.getElementById('circleText');
 var footer = document.getElementById('footer');
 
 $(document).ready(function ($) {
@@ -21,11 +22,13 @@ document.getElementById('btnStart').addEventListener('click', function () {
     if (id.length === 1 && (id === 'A' || id === 'B' || id === 'C' || id === 'D' || id === 'E'
         || id === 'F' || id === 'G' || id === 'H' || id === 'I' || id === 'J')) {
 
+        btnStart.style.color = 'black';
+
         fetch('/start', { method: 'POST', body: id })
             .then(response => response.text())
             .then(text => { textDisplay.innerText = text })
 
-    } else { textDisplay.innerText = 'Enter a Machine ID above!'; textDisplay.style.color="red" }
+    } else { textDisplay.innerText = 'Enter a Machine ID above!'; btnStart.style.color = 'red'; circleText.style.color='red'; }
 
 });
 
@@ -35,11 +38,13 @@ document.getElementById('btnFinish').addEventListener('click', function () {
     if (id.length === 1 && (id === 'A' || id === 'B' || id === 'C' || id === 'D' || id === 'E'
         || id === 'F' || id === 'G' || id === 'H' || id === 'I' || id === 'J')) {
 
+        btnFinish.style.color = 'black';
+
         fetch('/finish', { method: 'POST', body: id })
             .then(response => response.text())
             .then(text => { textDisplay.innerText = text })
 
-    } else { textDisplay.innerText = 'Enter a Machine ID above!' }
+    } else { textDisplay.innerText = 'Enter a Machine ID above!'; btnFinish.style.color = 'red'; circleText.style.color='red'; }
 
 });
 
@@ -53,7 +58,7 @@ footer.addEventListener('click', function () {
             .then(response => response.text())
             .then(text => { textDisplay.innerText = text })
 
-    } else { textDisplay.innerText = 'Enter a Machine ID above!' }
+    } else { textDisplay.innerText = 'Enter a Machine ID above!'; }
 
 });
 
